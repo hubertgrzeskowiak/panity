9385be9190f6964af0d9dfaf349eb2ca0b645096
Hubert Grzeskowiak aka Nemesis#13 <dev at nemesis13.de>
2012-04-04 00:14:26 +0200

    added a half done loadLevelAdditive() method

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 class BuildSettings(object):
-    # format: ?
+    # list of panda3d.core.Filename
     scenes = []
+
+class LevelLoadingError(Exception):
+    pass
 
 class Application(object):
     @staticmethod
@@ -8,5 +11,31 @@ class Application(object):
         # iterate over all objects in the current scene
         # check the attribute "dont_destroy_on_load"
         # destroy those objects which do not have that attribute
-        # 
-        pass
+        # call loadLevelAdditive()
+        raise NotImplementedError
+    
+    @staticmethod
+    def loadLevelAdditive(name_or_index):
+        # first try to use arg as index
+        try:
+            scene = BuildSettings.scenes[name_or_index]
+        except TypeError:
+            # then as full filename
+            for s in BuildSettings.scenes:
+                if s.getBasename == name_or_index:
+                    print s
+                    scene = s
+                    break
+            if not scene:
+                # then as basename (without extension)
+                for s in BuildSettings.scenes:
+                    if s.getBasenameWoExtension == name_or_index:
+                        print s
+                        scene = s
+                        break
+        if not scene:
+            raise LevelLoadingError("level {} couldn't be found".format(
+                                                                 name_or_index))
+        
+        if s.getExtension() in ("xml", "scene"):
+            scene = XMLSceneParser(s)
