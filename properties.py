0d4ad199682ebf6cc91ed1663543a431263d14f3
Hubert Grzeskowiak aka Nemesis#13 <dev at nemesis13.de>
2012-04-14 12:04:29 +0200

    properties now should work properly

--- properties.py
+++ properties.py
@@ -5,15 +5,27 @@ These are used to
 """
 
 class SerializedProperty(object):
-    def __init__(self):
-        self.value = None
+    def __init__(self, default=None):
+        self.default = default
+
     def __get__(self, obj, cls=None):
-        return self.value
+        if hasattr(obj, "_properties") and obj._properties.has_key(self):
+            return obj._properties[self]
+        else:
+            return self.default
+
     def __set__(self, obj, value):
-        # first check
+        """Set a property on an object. self.check is run and on failure
+        self.convert, followed by a second check. If one of the checks is
+        successful, the new value is returned, otherwise False.
+        """
+        # create a properties dict on the object
+        if not hasattr(obj, "_properties"):
+            obj._properties = {}
+        # check the given value
         try:
             self.check(value)
-            self.value = value
+            obj._properties[self] = value
             return value
         except AssertionError:
             pass
@@ -27,10 +39,10 @@ class SerializedProperty(object):
             return False
 
     def check(self, value):
-        """you should override this function.
-        
-        check a potential value for its correctness and type.
-        assertionerror is thrown if you're unlucky.
+        """Check a potential value for its correctness and optionally type.
+        AssertionError is thrown if you're unlucky.
+
+        Override this method in derived classes.
         """
         pass
     
@@ -39,45 +51,31 @@ class SerializedProperty(object):
         This is in particular useful when working with numbers.
         A converted value is returned that can be assigned to this property or
         None.
+
+        Override this method in derived classes.
         """
         pass
 
 
-class Component(SerializedProperty):
+class ComponentProperty(SerializedProperty):
     def __init__(self, typ=None):
         SerializedProperty.__init__(self)
         self.typ = typ
 
     def check(self, value):
+        assert hasattr(value, "name")
         assert hasattr(value, "game_object")
 
 
 class FloatProperty(SerializedProperty):
-    def __init__(self, minimum=None, maximum=None, default=None):
+    def __init__(self, minimum=None, maximum=None, default=0.0):
+        """When setting values to this, minimum and maximum are checked and the
+        given value might get clipped eventually. The default value is not
+        checked.
+        """
+        SerializedProperty.__init__(self, default)
         self.minimum = minimum
         self.maximum = maximum
-        self.default = default
-        assert minimum <= maximum
-        if default is not None:
-            self.check(default)
-            self.value = default
-        else:
-            if minimum is not None:
-                self.check(minimum)
-                self.value = minimum
-            elif maximum is not None:
-                self.check(maximum)
-                self.value = maximum
-            else:
-                self.value = 0.0
-    
-    def __copy__(self):
-        f = Float()
-        f.minimum = self.minimum
-        f.maximum = self.maximum
-        f.default = self.default
-        f.value = self.value
-        return f
 
     def check(self, value):
         assert isinstance(value, float)
@@ -96,3 +94,11 @@ class FloatProperty(SerializedProperty):
         elif self.maximum is not None and value > self.maximum:
             return self.maximum
         return value
+ 
+    def __copy__(self):
+        f = Float()
+        f.minimum = self.minimum
+        f.maximum = self.maximum
+        f.default = self.default
+        f.value = self.value
+        return f
