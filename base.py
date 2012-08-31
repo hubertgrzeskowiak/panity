f57a1f40142d6cb3cca783ba3a5f8acd333c32af
Hubert Grzeskowiak aka Nemesis#13 <dev at nemesis13.de>
2012-08-31 17:23:04 +0200

    removed legacy stuff

--- base.py
+++
@@ -1,7 +0,0 @@
-"""this is a global share module, sorta.
-only thing it does is holding an instance of ShowBase, that gets instanced
-as soon as this module is imported.
-"""
-
-from showbase import ShowBase
-base = ShowBase()
