0285fde7c0ade72a03811b24abed63c17742e1cf
Hubert Grzeskowiak <dev_ANTISPAM_@nemesis13.de>
2012-10-07 22:54:07 +0200

    various fixes to gameobject

--- gameobject.py
+++ gameobject.py
@@ -16,7 +16,7 @@ class GameObject(Object):
         self.components = {}
         self.transform = Transform(self)
         for component in components:
-            self.addComponent(component)    
+            self.addComponent(component)
 
     def getChildren(self):
         """Return a list of children as game objects."""
@@ -31,33 +31,24 @@ class GameObject(Object):
         """component can be class name or class. Instances of components are
         not allowed. The newly added component object is returned.
         """
-        if component in self.components:
-            raise AttributeError("game object {} already has a {} "+\
-                "component".format(self.transform.name, str(component)))
         if type(component) in types.StringTypes:
+            if component in self.components:
+                raise AttributeError("game object {} already has a {} "+\
+                    "component".format(self.transform.name, str(component)))
             comp_module = __import__("components."+component.lower(), globals(), locals(), [None])
             comp_class = getattr(comp_module, component)
             comp_object = comp_class(self)
             self.components[component] = comp_object
         else:
+            # it's a class
+            comp_name = component.__name__
+            if comp_name in self.components:
+                raise AttributeError("game object {} already has a {} "+\
+                    "component".format(self.transform.name, comp_name))
             comp_object = component()
-            self.components.append(comp_object)
+            self.components[comp_name] = comp_object
         return comp_object
 
     def getComponent(self, component):
         if self.components.has_key(component):
             return self.components[component]
-
-    @property
-    def transform(self):
-        return self._transform
-
-    @transform.setter
-    def transform(self, transform):
-        """transform is an instance of the Transform component.
-        This copies all properties of the passed transforms onto the current one.
-        """
-        if getattr(self, "_transform", None):
-            self._transform._destroy()
-        self._transform = transform
-        self.components["Transform"] = transform
