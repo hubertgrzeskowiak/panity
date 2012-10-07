6fd432acf1abf9c2d3b63b3b2eb585a15634c1b8
Hubert Grzeskowiak <dev_ANTISPAM_@nemesis13.de>
2012-10-07 17:34:32 +0200

    several extensions for more flexible and extendible functions

--- xmlsceneparser.py
+++ xmlsceneparser.py
@@ -20,40 +20,55 @@ COMPONENT_MODULES = [name for _, name, _ in pkgutil.iter_modules([_pkgpath])]
 class XMLSceneParser(ParserInterface):
     """An xml parser for scenes."""
     @staticmethod
-    def read(source, validate=True):
-        """Read a scene from an xml file, create it and return a root game object.
+    def getSceneFromXML(xml, validate=True):
+        """Read a scene from xml code or an xml file (xml argument ends
+        with '.xml'), create it and return a root game object.
         AssertionErrors can be thrown when validation fails.
         """
         # TODO: get the path from vfs and os-specific (appropriate for etree)
-        xmlscene = etree.parse(source)
+
+        if xml.endswith(".xml"):
+            xmlscene = etree.parse(xml)
+            root = GameObject(xml.rsplit(".xml", 1)[0])
+        else:
+            xmlscene = etree.fromstring(xml)
+            root = GameObject("scene")
+
         if validate:
             validateScene(xmlscene.getroot())
-        root = GameObject(source.rsplit(".xml", 1)[0])
+
         for xml_go in xmlscene.getroot():
-            go = XMLSceneParser._appendGameObject(xml_go)
+            go = XMLSceneParser.getGameObjectFromXML(xml_go)
             go.transform.parent = root.transform
+        
         return root
 
     @staticmethod
-    def _appendGameObject(xml_child, parent=None):
+    def getGameObjectFromXML(xml):
         """Create a game object from xml and append it as child of the parent
         game object. This function calls itself recursively. Prefabs are loaded
-        using self._getGameObjectFromPrefab().
+        using self.getGameObjectFromPrefab().
+        The xml parameter can be either a string, in which case it will be
+        parsed as xml or an etree.Element.
         """
 
+        if isinstance(xml, str):
+            xml = etree.fromstring(xml)
+
         # prefab means we should create an instance of a prefab and extend it
-        prefab = xml_child.get("prefab")
-        name = xml_child.get("name")
+        prefab = xml.get("prefab")
+        name = xml.get("name") or "unnamed"
         if prefab:
-            go = XMLSceneParser._getGameObjectFromPrefab(prefab, name)
+            go = XMLSceneParser.getGameObjectFromPrefab(prefab, name)
         else:
             # prefab attribute not set. Use a new game object
             go = GameObject(name=name)
-        for go_or_comp in xml_child:
+        for go_or_comp in xml:
             # a game object can contain game objects and components
             if go_or_comp.tag == "gameobject":
                 xml_go = go_or_comp
-                XMLSceneParser._appendGameObject(xml_go, go)
+                child = XMLSceneParser.getGameObjectFromXML(xml_go)
+                child.parent = go
             else: # must be a component otherwise
                 xml_comp = go_or_comp
                 # does the GO already have such a component?
@@ -73,15 +88,12 @@ class XMLSceneParser(ParserInterface):
                     setattr(comp, option, option.text)
 
         go.name = name
-        if parent:
-            go.transform.parent = parent.transform
-        else:
-            return go
+        return go
 
     @staticmethod
-    def _getGameObjectFromPrefab(prefab_source, name):
+    def getGameObjectFromPrefab(prefab_source, name):
         """Create a game object from a prefab, which corresponds to a
-        prefab file from the prefabs package.
+        prefab file from the prefabs package or something.
         """
         raise NotImplementedError
         # TODO:
@@ -92,9 +104,14 @@ class XMLSceneParser(ParserInterface):
         # call PrefabUtility.getPrefabObject()
 
     @staticmethod
-    def write(source, data):
-        """Write a scene to xml file. data is an iterable of the root game
-        objects.
-        """
-        # TODO
+    def getXMLFromScene(scene,):
+        """Serialize a whole scene to xml code."""
         pass
+
+    @staticmethod
+    def getXMLFromGameObject(game_object):
+        pass
+
+    @staticmethod
+    def getXMLFromComponent(component):
+        print component.getSerializedProperties()
